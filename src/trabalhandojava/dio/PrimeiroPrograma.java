ca1ec8a386aef482c0971b5e90b8934f97dfc0c0
Charbio Silva <95384632+Charbinho86@users.noreply.github.com>
2022-01-09 13:47:24 -0300

    Adiciona novo arquivo calculadora e atualiza o arquivo gato

--- src/trabalhandojava/dio/PrimeiroPrograma.java
+++ src/trabalhandojava/dio/PrimeiroPrograma.java
@@ -1,5 +1,7 @@
 package trabalhandojava.dio;
 
+import trabalhandojava.dio.gato.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
---
+++ src/trabalhandojava/dio/calculadora/Calculadora.java
@@ -0,0 +1,44 @@
+package trabalhandojava.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a, b;
+			
+			System.out.println("Digite o primeiro número: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo número: ");
+			b = scan.nextInt();
+			
+			int soma = soma(a,b);
+			int subtracao = subtracao(a,b);
+			int multiplicacao = multiplicacao(a,b);
+			float divisao = divisao(a,b);
+			
+			System.out.println("Soma = " + a + " + " + b + " = "  + soma);
+			System.out.println("Sub = " +  a  + " - " +  b  + " = " + subtracao);
+			System.out.println("Mult = " + a + " * " + b + " = " + multiplicacao);
+			System.out.println("Div = " + a + " / " + b + " = " + divisao);
+		}
+		
+	}
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	 
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	
+	public static float divisao(float a, float b) {
+		return a / b;
+	}
+}
